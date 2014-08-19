1991f9caf09de55426bab3f34d5608fe89fae148
Chris Ondrovic <chris.ondrovic@inin.com>
2014-08-19 17:58:02 -0400

    Updates

--- ServerReboot/Validations/ManagerValidaioncs.cs
+++ ServerReboot/Validations/ManagerValidaioncs.cs
@@ -9,6 +9,14 @@ namespace ServerReboot.Validations
 
         public string Manger { get { return _manager; } set { _manager = value; } }
 
+        /// <summary>
+        /// When overridden in a derived class, performs validation checks on a value.
+        /// </summary>
+        /// <param name="value">The value from the binding target to check.</param>
+        /// <param name="cultureInfo">The culture to use in this rule.</param>
+        /// <returns>
+        /// A <see cref="T:System.Windows.Controls.ValidationResult" /> object.
+        /// </returns>
         public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
         {
             string manager = string.Empty;
--- ServerReboot/View/View.cs
+++ ServerReboot/View/View.cs
@@ -2,6 +2,9 @@
 
 namespace ServerReboot
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class View
     {
         public View()
@@ -12,8 +15,26 @@ namespace ServerReboot
         private string _server;
         private string _rfc;
         private string _manager;
+        /// <summary>
+        /// Gets or sets the name of the server.
+        /// </summary>
+        /// <value>
+        /// The name of the server.
+        /// </value>
         public string ServerName { get { return _server; } set { _server = value; } }
+        /// <summary>
+        /// Gets or sets the RFC number.
+        /// </summary>
+        /// <value>
+        /// The RFC number.
+        /// </value>
         public string RFCNumber { get { return _rfc; } set { _rfc = value; } }
+        /// <summary>
+        /// Gets or sets the manager.
+        /// </summary>
+        /// <value>
+        /// The manager.
+        /// </value>
         public string Manager { get { return _manager; } set { _manager = value; } }
 
     }
